3db2132bd8e9915a986146c2343ecb29dc769584
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2023-03-01 15:04:05 +0100

    added u_urlOpen.pas to the project

--- src/PromoVerif.dpr
+++ src/PromoVerif.dpr
@@ -5,7 +5,8 @@ uses
   FMX.Forms,
   fMain in 'fMain.pas' {frmMain},
   uDm in 'uDm.pas' {dm: TDataModule},
-  FMX.DzHTMLText in '..\lib-externes\DzHTMLText\FMX.DzHTMLText.pas';
+  FMX.DzHTMLText in '..\lib-externes\DzHTMLText\FMX.DzHTMLText.pas',
+  u_urlOpen in '..\lib-externes\librairies\u_urlOpen.pas';
 
 {$R *.res}
 
